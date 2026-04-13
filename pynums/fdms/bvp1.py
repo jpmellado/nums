8924737cf2593284f4a4fa5cd6c2ee0cd6128215
Juan Pedro Mellado <mellado@gmx.de>
2026-04-13 15:46:50 +0200

    Testing boundary value module

--- pynums/fdms/bvp1.py
+++ pynums/fdms/bvp1.py
@@ -3,24 +3,30 @@ import scipy.linalg
 
 # Inverts a finite-difference approximation to solve a boundary value problem.
 
+
 def bvp1_e11b(f):
-    # Solves the BVP 
-    #       u'_j=f_j,   j = 2,...n 
+    # Solves the BVP
+    #       u'_j=f_j,   j = 2,...n
     #       u_1 = 0
     # using a explicit formulation with backward biased formulas
     # Still need to multiply by the grid spacing h
     # Input arguments:
     #    - f: values of the forcing at the grid points
     # Output arguments:
-    #    -u: solution at the grid points 
+    #    -u: solution at the grid points
     n = np.size(f)
     u = np.zeros_like(f)
 
     # Create system array
-    A = np.diag(np.full(n-1, 1.))                       # Create array and fill main diagonal
-    np.fill_diagonal(A[1:,:],np.full(n-2,-1.))          # Fill lower diagonal
+    # A = np.diag(np.full(n-1, 1.))                       # Create array and fill main diagonal
+    # np.fill_diagonal(A[1:,:],np.full(n-2,-1.))          # Fill lower diagonal
 
-    # Solve the system. We use generic routines, but one could use solve_banded
-    u[1:] = scipy.linalg.solve( A, f[1:] )
+    # # Solve the system. We use generic routines, but one could use solve_banded
+    # u[1:] = scipy.linalg.solve( A, f[1:] )
+
+    # The snippet before is for a general case; in this particular case, the linear system is very easy
+    # and can be solved directly:
+    for i in range(1, n):
+        u[i] = u[i - 1] + f[i]
 
     return u
